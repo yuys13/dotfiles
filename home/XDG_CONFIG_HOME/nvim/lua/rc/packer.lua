58808efd425df7bcd55736b754ad1d07333e104f
yuys13 <yuys13@users.noreply.github.com>
2021-12-28 00:59:14 +0900

    Add nvim-tree

--- home/XDG_CONFIG_HOME/nvim/lua/rc/packer.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/packer.lua
@@ -311,6 +311,16 @@ local init = function()
   }
 
   use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  }
+
+  use {
     'cohama/lexima.vim',
   }
 end

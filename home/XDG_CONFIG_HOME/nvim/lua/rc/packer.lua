786ecb2979cd1c1d72cf63aa05f81e565973abac
yuys13 <yuys13@users.noreply.github.com>
2021-12-28 23:47:39 +0900

    Update tree-sitter settings

--- home/XDG_CONFIG_HOME/nvim/lua/rc/packer.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/packer.lua
@@ -159,7 +159,41 @@ local init = function()
     end,
   }
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+        ensure_installed = 'maintained',
+
+        -- Install languages synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- List of parsers to ignore installing
+        -- ignore_install = { 'javascript' },
+
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
+
+          -- list of language that will be disabled
+          -- disable = { 'c', 'rust' },
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end,
+  }
+
+  use {
+    'nvim-treesitter/playground',
+    disable = true,
+  }
 
   use { 'dracula/vim', as = 'dracula', config = 'vim.cmd[[colorscheme dracula]]' }
   use { 'cocopon/iceberg.vim', opt = true }

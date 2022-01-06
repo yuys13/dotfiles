ef4c42196b5a65812ebb743f0f2506c3ef740aa7
yuys13 <yuys13@users.noreply.github.com>
2022-01-06 22:12:19 +0900

    Add lspkind

--- home/XDG_CONFIG_HOME/nvim/lua/rc/nvim-cmp.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/nvim-cmp.lua
@@ -1,5 +1,6 @@
 -- nvim-cmp setup
 local cmp = require 'cmp'
+local lspkind = require 'lspkind'
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -50,6 +51,18 @@ cmp.setup {
     { name = 'path' },
     { name = 'buffer' },
   }),
+  formatting = {
+    format = lspkind.cmp_format {
+      with_text = true, -- do not show text alongside icons
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      -- before = function (entry, vim_item)
+      --   return vim_item
+      -- end
+    },
+  },
 }
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
--- home/XDG_CONFIG_HOME/nvim/lua/rc/packer.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/packer.lua
@@ -34,6 +34,7 @@ local init = function()
       { 'hrsh7th/cmp-buffer' },
       { 'hrsh7th/cmp-path' },
       { 'hrsh7th/cmp-cmdline' },
+      { 'onsails/lspkind-nvim' },
     },
   }
 

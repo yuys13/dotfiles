9d8b91692381051ffb811dee68ae717dfe4fa578
yuys13 <yuys13@users.noreply.github.com>
2022-01-17 00:30:19 +0900

    Update nvim-cmp settings

--- home/XDG_CONFIG_HOME/nvim/lua/rc/nvim-cmp.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/nvim-cmp.lua
@@ -12,16 +12,14 @@ cmp.setup {
     end,
   },
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    -- ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    -- ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<C-f>'] = cmp.mapping(cmp.mapping.confirm { select = true }, { 'i' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    },
-    -- ['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<CR>'] = cmp.mapping.confirm { select = false },
+    ['<C-e>'] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
+    -- ['<CR>'] = cmp.mapping.confirm { select = true },
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
@@ -35,16 +33,17 @@ cmp.setup {
   }),
   formatting = {
     format = lspkind.cmp_format {
-      with_text = true, -- do not show text alongside icons
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      -- before = function (entry, vim_item)
-      --   return vim_item
-      -- end
+      with_text = true,
+      menu = {
+        buffer = '[Buffer]',
+        -- cmdline = '[CmdLine]',
+        nvim_lsp = '[LSP]',
+        path = '[Path]',
+        vsnip = '[Vsnip]',
+      },
     },
   },
+  experimental = { ghost_text = true },
 }
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).

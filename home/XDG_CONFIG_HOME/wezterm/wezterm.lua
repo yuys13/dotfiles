ca8fabce102515ac9e2aa1d52ef8676730234c5c
yuys13 <yuys13@users.noreply.github.com>
2022-04-04 21:04:33 +0900

    Add tab settings for wezterm

--- home/XDG_CONFIG_HOME/wezterm/wezterm.lua
+++ home/XDG_CONFIG_HOME/wezterm/wezterm.lua
@@ -1,5 +1,25 @@
 local wezterm = require 'wezterm'
 
+local function basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
+local function convert_home_dir(path)
+  local cwd = path
+  cwd = cwd:gsub('^' .. wezterm.home_dir, '~')
+  return cwd
+end
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local proc = basename(tab.active_pane.foreground_process_name)
+  local cwd = tab.active_pane.current_working_dir:gsub('^file://', '')
+  cwd = basename(convert_home_dir(cwd))
+  local title = tab.tab_index + 1 .. ':' .. cwd .. ' | ' .. proc
+  return {
+    { Text = wezterm.truncate_right(title, max_width) },
+  }
+end)
+
 wezterm.on('update-right-status', function(window, pane)
   local date = wezterm.strftime '%Y-%m-%d %H:%M:%S'
 
@@ -18,6 +38,8 @@ if status then
 else
   M.color_scheme = 'Dracula'
 end
+
+M.tab_max_width = 24
 
 M.adjust_window_size_when_changing_font_size = false
 -- M.hide_tab_bar_if_only_one_tab = true

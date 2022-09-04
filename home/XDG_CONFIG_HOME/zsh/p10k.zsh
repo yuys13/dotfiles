4095978137b59b4917ffe73816e681f8fc6f59df
yuys13 <yuys13@users.noreply.github.com>
2022-09-04 22:51:09 +0900

    Enable shell integration

--- home/XDG_CONFIG_HOME/zsh/p10k.zsh
+++ home/XDG_CONFIG_HOME/zsh/p10k.zsh
@@ -1650,3 +1650,6 @@ typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
 (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
 'builtin' 'unset' 'p10k_config_opts'
+
+# Enable shell integration
+typeset -g POWERLEVEL9K_TERM_SHELL_INTEGRATION=true

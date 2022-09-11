13d2c4404815e34ea15a70780c59de51a134b3ec
yuys13 <yuys13@users.noreply.github.com>
2022-09-11 21:12:51 +0900

    Fix fish alias

--- home/XDG_CONFIG_HOME/fish/functions/vi.fish
+++ home/XDG_CONFIG_HOME/fish/functions/vi.fish
@@ -1,6 +1,6 @@
 # Defined in - @ line 1
 function vi --wraps='$EDITOR' --description 'alias vi $EDITOR'
-    if test "$EDITOR" = vi
+    if test -z "$EDITOR"
         command vi $argv
     else
         eval $EDITOR $argv

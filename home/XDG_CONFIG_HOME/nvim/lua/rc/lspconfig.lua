d337a9a7b6fd9a4ff5ef4ad3bbddee699c4a3903
yuys13 <yuys13@users.noreply.github.com>
2022-10-06 21:26:23 +0900

    chore(nvim): use the default settings of neovim 0.8

--- home/XDG_CONFIG_HOME/nvim/lua/rc/lspconfig.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/lspconfig.lua
@@ -1,9 +1,6 @@
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.bo.omnifunc = 'v:lua.vim.lsp.omnifunc'
-
   -- Mappings.
   local opts = { buffer = true, silent = true }
 

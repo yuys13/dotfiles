67492c9aa35c817ef54613afd420e725b9b8cc30
yuys13 <yuys13@users.noreply.github.com>
2022-10-09 23:24:30 +0900

    feat(zsh): use dracula theme for fzf

--- home/XDG_CONFIG_HOME/zsh/plugins.zsh
+++ home/XDG_CONFIG_HOME/zsh/plugins.zsh
@@ -86,6 +86,9 @@
             FZF_CDR_PREVIEW_OPTS="ls --color "
         fi
 
+        if [[ ${COLORTERM} = 'truecolor' ]]; then
+            FZF_DEFAULT_OPTS='--layout=reverse --height 40% --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+        fi
         export FZF_DEFAULT_OPTS=${FZF_DEFAULT_OPTS:-"--layout=reverse --height 40%"}
         function bd_list () {
             local dir=$PWD

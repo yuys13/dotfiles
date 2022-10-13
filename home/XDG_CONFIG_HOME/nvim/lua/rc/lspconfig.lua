69bf4081b8e6d83e2055d54899898932d045d823
yuys13 <yuys13@users.noreply.github.com>
2022-10-13 21:56:28 +0900

    chore: add sumneko_lua config for reading-vimrc

--- home/XDG_CONFIG_HOME/nvim/lua/rc/lspconfig.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/lspconfig.lua
@@ -53,6 +53,35 @@ local enhance_server_opts = {
       },
     }
   end,
+  -- for reading-vimrc
+  -- ['sumneko_lua'] = function(opts)
+  --   local runtime_path = vim.split(package.path, ';')
+  --   table.insert(runtime_path, 'lua/?.lua')
+  --   table.insert(runtime_path, 'lua/?/init.lua')
+  --
+  --   opts.settings = {
+  --     Lua = {
+  --       runtime = {
+  --         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the
+  --         version = 'LuaJIT',
+  --         -- Setup your lua path
+  --         path = runtime_path,
+  --       },
+  --       diagnostics = {
+  --         -- Get the language server to recognize the `vim` global
+  --         globals = { 'vim' },
+  --       },
+  --       workspace = {
+  --         -- Make the server aware of Neovim runtime files
+  --         library = vim.api.nvim_get_runtime_file('', true),
+  --       },
+  --       -- Do not send telemetry data containing a randomized but unique identifier
+  --       telemetry = {
+  --         enable = false,
+  --       },
+  --     },
+  --   }
+  -- end,
 }
 
 require('lua-dev').setup {}

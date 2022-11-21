a6d9a7cbc9da6db214522215415b637138e49f16
yuys13 <yuys13@users.noreply.github.com>
2022-11-21 12:59:34 +0900

    feat(nvim): update treesitter disable function

--- home/XDG_CONFIG_HOME/nvim/lua/rc/packer.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/packer.lua
@@ -281,11 +281,25 @@ local init = function()
           --     return true
           --   end
           -- end,
-          disable = function(lang)
-            local ok = pcall(function()
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+            if not pcall(function()
+              vim.treesitter.get_parser(0, lang):parse()
+            end) then
+              return true
+            end
+
+            if not pcall(function()
               vim.treesitter.get_query(lang, 'highlights')
-            end)
-            return not ok
+            end) then
+              return true
+            end
+
+            return false
           end,
 
           -- Setting this to true will run `:h syntax` and tree-sitter at the same time.

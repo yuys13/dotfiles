8e42fde27a67a997ab4eabbd1b114da866f1be7e
yuys13 <yuys13@users.noreply.github.com>
2023-01-10 18:18:30 +0900

    feat(nvim): remove nvim-treesitter-context

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-treesitter.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-treesitter.lua
@@ -115,12 +115,6 @@ return {
     end,
     dependencies = {
       { 'nvim-treesitter/nvim-treesitter-textobjects' },
-      {
-        'nvim-treesitter/nvim-treesitter-context',
-        config = function()
-          require('treesitter-context').setup {}
-        end,
-      },
     },
   },
 

fc7507e947aeaa4b72004636a227700a9f7be1af
yuys13 <yuys13@users.noreply.github.com>
2023-01-14 23:07:16 +0900

    fix(nvim): fix event to load Comment.nvim

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-treesitter.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-treesitter.lua
@@ -125,7 +125,7 @@ return {
 
   {
     'numToStr/Comment.nvim',
-    event = { 'BufRead', 'BufNewFile' },
+    keys = { 'gc', 'gb' },
     config = function()
       require('Comment').setup {
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),

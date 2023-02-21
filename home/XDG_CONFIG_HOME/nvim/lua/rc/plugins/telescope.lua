f78fc4692d23d32e637f993ec3eff7e6c77ae211
yuys13 <yuys13@users.noreply.github.com>
2023-02-22 00:06:44 +0900

    feat(nvim): use emacs like keymap for telescope

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/telescope.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/telescope.lua
@@ -30,7 +30,8 @@ return {
 
       map('n', '<Space>/', '<Cmd>Telescope current_buffer_fuzzy_find<CR>', { desc = 'Telescope current_buffer_ff' })
       map('n', '<Space>f/', '<Cmd>Telescope live_grep<CR>', { desc = 'Telescope live_grep' })
-      map('n', '<Space><Space>', '<Cmd>Telescope builtin<CR>', { desc = 'Telescope builtin' })
+      map('n', '<M-x>', '<Cmd>Telescope builtin<CR>', { desc = 'Telescope builtin' })
+      map('n', '<C-x><C-f>', '<Cmd>Telescope file_browser<CR>', { desc = 'Telescope file_browser' })
     end,
     config = function()
       require('telescope').setup {

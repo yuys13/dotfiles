b9b27f7322da168ddc3d43461334019d1d8faf95
yuys13 <yuys13@users.noreply.github.com>
2023-04-05 12:38:40 +0900

    fix(nvim): library not loading outside of dotfiles

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-lsp.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-lsp.lua
@@ -136,7 +136,7 @@ local spec = {
                 workspace = {
                   checkThirdParty = false,
                   -- Make the server aware of Neovim runtime files
-                  -- library = vim.api.nvim_get_runtime_file('', true),
+                  library = vim.api.nvim_get_runtime_file('lua', true),
                 },
               },
             },

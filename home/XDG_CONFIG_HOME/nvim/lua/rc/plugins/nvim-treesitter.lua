39ad4bf94579709adc3197baf435c1aae7136dc2
yuys13 <yuys13@users.noreply.github.com>
2024-05-27 12:38:42 +0900

    update(nvim): use indent module of nvim-treesitter

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-treesitter.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-treesitter.lua
@@ -92,6 +92,9 @@ local spec = {
           -- Instead of true it can also be a list of languages
           additional_vim_regex_highlighting = false,
         },
+        indent = {
+          enable = true,
+        },
         textobjects = {
           select = {
             enable = true,

4f4234407afc3b76b955e9528e7f2a93b925a57f
yuys13 <yuys13@users.noreply.github.com>
2024-06-03 17:40:17 +0900

    fix(nvim): update lazydev settings

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-lsp.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-lsp.lua
@@ -224,7 +224,12 @@ local spec = {
     config = function()
       require('lazydev').setup {
         library = {
-          vim.env.LAZY .. '/luvit-meta/library',
+          -- Library items can be absolute paths
+          -- "~/projects/my-awesome-lib",
+          -- Or relative, which means they will be resolved as a plugin
+          -- "LazyVim",
+          -- When relative, you can also provide a path to the library in the plugin dir
+          'luvit-meta/library', -- see below
         },
       }
     end,

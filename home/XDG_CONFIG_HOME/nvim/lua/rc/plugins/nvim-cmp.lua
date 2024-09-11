e74c72bb1f55bbd1a594b04d2c304adef96fa9bf
yuys13 <yuys13@users.noreply.github.com>
2024-09-11 20:27:21 +0900

    chore(nvim): remove unnecessary function

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-cmp.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-cmp.lua
@@ -27,10 +27,6 @@ local spec = {
       -- nvim-cmp setup
       local cmp = require 'cmp'
 
-      local feedkey = function(key, mode)
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-      end
-
       cmp.setup {
         preselect = cmp.PreselectMode.None,
         snippet = {

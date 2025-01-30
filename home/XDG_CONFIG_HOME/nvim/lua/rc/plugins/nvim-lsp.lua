89b74465990b3dbb5c95d7efbfd1d2f73d0178c6
yuys13 <yuys13@users.noreply.github.com>
2025-01-30 12:24:44 +0900

    feat(nvim): add type annotation for test tools

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-lsp.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-lsp.lua
@@ -249,11 +249,15 @@ local spec = {
           'lazy.nvim',
           'luvit-meta/library', -- see below
           'plenary.nvim',
+          '${3rd}/busted/library',
+          '${3rd}/luassert/library',
         },
       }
     end,
   },
   { 'Bilal2453/luvit-meta', lazy = true },
+  -- { 'LuaCATS/busted', lazy = true },
+  -- { 'LuaCATS/luassert', lazy = true },
 
   {
     'ray-x/lsp_signature.nvim',

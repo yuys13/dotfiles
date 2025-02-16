bc71073a88e79665705cd451043372ccf965a5f8
yuys13 <yuys13@users.noreply.github.com>
2025-02-16 22:27:29 +0900

    feat(nvim): use snacks.picker instead of telescope

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/snacks.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/snacks.lua
@@ -3,6 +3,27 @@ local spec = {
   {
     'folke/snacks.nvim',
     lazy = false,
+    keys = {
+      -- stylua: ignore start
+      { '<Space><Space>', function() Snacks.picker() end, desc = 'Picker' },
+      { '<Space>fh', function() Snacks.picker.help() end, desc = 'Help' },
+      { '<Space>ff', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
+      { '<Space>f/', function() Snacks.picker.grep() end, desc = 'Grep' },
+      { '<Space>f:', function() Snacks.picker.command_history() end, desc = 'Command History' },
+      -- { "<Space>fn", function() Snacks.picker.notifications() end, desc = "Notification History" },
+      -- { "<Space>fe", function() Snacks.explorer() end, desc = "File Explorer" },
+      { '<Space>fr', function() Snacks.picker.resume() end, desc = 'Resume' },
+      -- find
+      { '<Space>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+      -- { "<Space>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+      -- { "<Space>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+      { '<Space>fg', function() Snacks.picker.git_files({untracked = true}) end, desc = 'Find Git Files' },
+      -- { "<Space>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+      { '<Space>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
+      -- Grep
+      { '<Space>/', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
+      -- stylua: ignore stop
+    },
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('snacks').setup {
--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/telescope.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/telescope.lua
@@ -3,38 +3,38 @@ local spec = {
   {
     'nvim-telescope/telescope.nvim',
     cmd = 'Telescope',
-    init = function()
-      local function map(mode, lhs, rhs, opts)
-        opts = opts or {}
-        opts.silent = true
-        vim.keymap.set(mode, lhs, rhs, opts)
-      end
-
-      map('n', '<Space>ff', '<Cmd>Telescope find_files<CR>', { desc = 'Telescope find_files' })
-      map('n', '<Space>fb', '<Cmd>Telescope buffers<CR>', { desc = 'Telescope buffers' })
-      map('n', '<Space>fr', '<Cmd>Telescope resume<CR>', { desc = 'Telescope resume' })
-      map('n', '<Space>fg', function()
-        require('telescope.builtin').git_files { git_command = { 'git', 'ls-files', '--exclude-standard', '-co' } }
-      end, { desc = 'Telescope git_files' })
-      map('n', '<Space>fig', function()
-        require('telescope.builtin').git_files { git_command = { 'git', 'ls-files', '--exclude-standard', '-coi' } }
-      end, { desc = 'Telescope git_files ignore only' })
-
-      map('n', '<Space>f*', '<Cmd>Telescope grep_string<CR>', { desc = 'Telescope grep_string' })
-
-      map('n', '<Space>fk', '<Cmd>Telescope keymaps<CR>', { desc = 'Telescope keymaps' })
-      map('n', '<Space>fh', '<Cmd>Telescope help_tags<CR>', { desc = 'Telescope help_tags' })
-      map('n', '<Space>fcs', '<Cmd>Telescope colorscheme<CR>', { desc = 'Telescope colorscheme' })
-      map('n', '<Space>fFT', '<Cmd>Telescope filetypes<CR>', { desc = 'Telescope filetypes' })
-
-      map('n', '<Space>f:', '<Cmd>Telescope command_history<CR>', { desc = 'Telescope command_history' })
-
-      map('n', '<Space>/', '<Cmd>Telescope current_buffer_fuzzy_find<CR>', { desc = 'Telescope current_buffer_ff' })
-      map('n', '<Space>f/', '<Cmd>Telescope live_grep<CR>', { desc = 'Telescope live_grep' })
-      map('n', '<M-x>', '<Cmd>Telescope builtin<CR>', { desc = 'Telescope builtin' })
-      map('n', '<Space><Space>', '<Cmd>Telescope builtin<CR>', { desc = 'Telescope builtin' })
-      map('n', '<Space>ft', '<Cmd>Telescope file_browser<CR>', { desc = 'Telescope file_browser' })
-    end,
+    -- init = function()
+    --   local function map(mode, lhs, rhs, opts)
+    --     opts = opts or {}
+    --     opts.silent = true
+    --     vim.keymap.set(mode, lhs, rhs, opts)
+    --   end
+    --
+    --   map('n', '<Space>ff', '<Cmd>Telescope find_files<CR>', { desc = 'Telescope find_files' })
+    --   map('n', '<Space>fb', '<Cmd>Telescope buffers<CR>', { desc = 'Telescope buffers' })
+    --   map('n', '<Space>fr', '<Cmd>Telescope resume<CR>', { desc = 'Telescope resume' })
+    --   map('n', '<Space>fg', function()
+    --     require('telescope.builtin').git_files { git_command = { 'git', 'ls-files', '--exclude-standard', '-co' } }
+    --   end, { desc = 'Telescope git_files' })
+    --   map('n', '<Space>fig', function()
+    --     require('telescope.builtin').git_files { git_command = { 'git', 'ls-files', '--exclude-standard', '-coi' } }
+    --   end, { desc = 'Telescope git_files ignore only' })
+    --
+    --   map('n', '<Space>f*', '<Cmd>Telescope grep_string<CR>', { desc = 'Telescope grep_string' })
+    --
+    --   map('n', '<Space>fk', '<Cmd>Telescope keymaps<CR>', { desc = 'Telescope keymaps' })
+    --   map('n', '<Space>fh', '<Cmd>Telescope help_tags<CR>', { desc = 'Telescope help_tags' })
+    --   map('n', '<Space>fcs', '<Cmd>Telescope colorscheme<CR>', { desc = 'Telescope colorscheme' })
+    --   map('n', '<Space>fFT', '<Cmd>Telescope filetypes<CR>', { desc = 'Telescope filetypes' })
+    --
+    --   map('n', '<Space>f:', '<Cmd>Telescope command_history<CR>', { desc = 'Telescope command_history' })
+    --
+    --   map('n', '<Space>/', '<Cmd>Telescope current_buffer_fuzzy_find<CR>', { desc = 'Telescope current_buffer_ff' })
+    --   map('n', '<Space>f/', '<Cmd>Telescope live_grep<CR>', { desc = 'Telescope live_grep' })
+    --   map('n', '<M-x>', '<Cmd>Telescope builtin<CR>', { desc = 'Telescope builtin' })
+    --   map('n', '<Space><Space>', '<Cmd>Telescope builtin<CR>', { desc = 'Telescope builtin' })
+    --   map('n', '<Space>ft', '<Cmd>Telescope file_browser<CR>', { desc = 'Telescope file_browser' })
+    -- end,
     config = function()
       require('telescope').setup {
         defaults = {

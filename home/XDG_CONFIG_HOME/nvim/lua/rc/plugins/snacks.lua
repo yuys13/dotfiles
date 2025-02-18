d04b1aa9fc43d3bc767ebc4fd4683045db8ef7f7
yuys13 <yuys13@users.noreply.github.com>
2025-02-18 12:41:24 +0900

    chore(nvim): update snacks config

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/snacks.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/snacks.lua
@@ -7,7 +7,7 @@ local spec = {
       -- stylua: ignore start
       { '<Space><Space>', function() Snacks.picker() end, desc = 'Picker' },
       { '<Space>fh', function() Snacks.picker.help() end, desc = 'Help' },
-      { '<Space>ff', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
+      { '<Space>ff', function() Snacks.picker.smart { multi = { 'buffers', 'files' } } end, desc = 'Smart Find Files' },
       { '<Space>f/', function() Snacks.picker.grep() end, desc = 'Grep' },
       { '<Space>f:', function() Snacks.picker.command_history() end, desc = 'Command History' },
       -- { "<Space>fn", function() Snacks.picker.notifications() end, desc = "Notification History" },
@@ -17,12 +17,12 @@ local spec = {
       { '<Space>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
       -- { "<Space>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
       -- { "<Space>ff", function() Snacks.picker.files() end, desc = "Find Files" },
-      { '<Space>fg', function() Snacks.picker.git_files({untracked = true}) end, desc = 'Find Git Files' },
+      { '<Space>fg', function() Snacks.picker.git_files { untracked = true } end, desc = 'Find Git Files' },
       -- { "<Space>fp", function() Snacks.picker.projects() end, desc = "Projects" },
       { '<Space>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
       -- Grep
       { '<Space>/', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
-      -- stylua: ignore stop
+      -- stylua: ignore end
     },
     config = function()
       ---@diagnostic disable-next-line: missing-fields

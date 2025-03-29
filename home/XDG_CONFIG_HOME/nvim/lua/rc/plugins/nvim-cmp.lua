301e639e28d45eb28e541bf10536139ab47bc16a
yuys13 <yuys13@users.noreply.github.com>
2025-03-29 15:37:49 +0900

    chore(nvim): use completion_item insted of get_completion_item

--- home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-cmp.lua
+++ home/XDG_CONFIG_HOME/nvim/lua/rc/plugins/nvim-cmp.lua
@@ -70,7 +70,7 @@ local spec = {
         formatting = {
           format = function(entry, vim_item)
             if vim.tbl_contains({ 'path' }, entry.source.name) then
-              local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+              local icon, hl_group = require('nvim-web-devicons').get_icon(entry.completion_item.label)
               if icon then
                 vim_item.kind = icon .. ' ' .. vim_item.kind
                 vim_item.kind_hl_group = hl_group

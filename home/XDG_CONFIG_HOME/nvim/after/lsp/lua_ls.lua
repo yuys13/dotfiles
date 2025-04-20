92bc93d3dab36258297d695df6d8f2c7d87372f3
yuys13 <yuys13@users.noreply.github.com>
2025-04-20 18:32:56 +0900

    fix(nvim): correct conditional check for workspace folders in lua_ls.lua

--- home/XDG_CONFIG_HOME/nvim/after/lsp/lua_ls.lua
+++ home/XDG_CONFIG_HOME/nvim/after/lsp/lua_ls.lua
@@ -3,9 +3,11 @@ return {
     client.server_capabilities.semanticTokensProvider = nil
   end,
   on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-      return
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+        return
+      end
     end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {

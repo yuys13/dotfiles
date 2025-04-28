36651ef7eb1ceb39266083ce1fd85bb2a07e6ea6
yuys13 <yuys13@users.noreply.github.com>
2025-04-28 21:33:52 +0900

    feat(nvim): use workspace_required

--- home/XDG_CONFIG_HOME/nvim/after/lsp/denols.lua
+++ home/XDG_CONFIG_HOME/nvim/after/lsp/denols.lua
@@ -1,16 +1,9 @@
 ---@type vim.lsp.Config
 return {
-  root_dir = function(bufnr, callback)
-    local found_dirs = vim.fs.find({
-      'deno.json',
-      'deno.jsonc',
-      'deps.ts',
-    }, {
-      upward = true,
-      path = vim.fs.dirname(vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr))),
-    })
-    if #found_dirs > 0 then
-      return callback(vim.fs.dirname(found_dirs[1]))
-    end
-  end,
+  root_markers = {
+    'deno.json',
+    'deno.jsonc',
+    'deps.ts',
+  },
+  workspace_required = true,
 }

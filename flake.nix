e432e9564d4096fe8a0bbf7d5a7fc5e6626682d7
yuys13 <yuys13@users.noreply.github.com>
2026-07-18 23:03:51 +0900

    feat(nix): migrate flake.nix to flake-parts and treefmt-nix

--- flake.nix
+++ flake.nix
@@ -11,21 +11,64 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{
-      self,
-      nix-darwin,
-      nixpkgs,
-      home-manager,
-    }:
     {
-      darwinConfigurations."eve24" = nix-darwin.lib.darwinSystem {
-        modules = [
-          ./nix/hosts/eve24/configuration.nix
-          home-manager.darwinModules.home-manager
-        ];
+      self,
+      flake-parts,
+      home-manager,
+      nix-darwin,
+      treefmt-nix,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+
+      flake = {
+        darwinConfigurations."eve24" = nix-darwin.lib.darwinSystem {
+          modules = [
+            ./nix/hosts/eve24/configuration.nix
+            home-manager.darwinModules.home-manager
+          ];
+        };
       };
+
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              stylua.enable = true;
+              yamlfmt.enable = true;
+              mdformat.enable = true;
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              config.treefmt.build.wrapper
+            ];
+          };
+
+        };
     };
 }

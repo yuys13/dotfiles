b41802120c3df75a24204902a14c55a56abaeab6
yuys13 <yuys13@users.noreply.github.com>
2026-09-21 00:04:44 +0900

    feat(nixos): configure extra locale settings

--- nix/nixos/default.nix
+++ nix/nixos/default.nix
@@ -14,7 +14,20 @@
 
   # Time zone and locale
   time.timeZone = "Asia/Tokyo";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "ja_JP.UTF-8";
+      LC_IDENTIFICATION = "ja_JP.UTF-8";
+      LC_MEASUREMENT = "ja_JP.UTF-8";
+      LC_MONETARY = "ja_JP.UTF-8";
+      LC_NAME = "ja_JP.UTF-8";
+      LC_NUMERIC = "ja_JP.UTF-8";
+      LC_PAPER = "ja_JP.UTF-8";
+      LC_TELEPHONE = "ja_JP.UTF-8";
+      LC_TIME = "ja_JP.UTF-8";
+    };
+  };
 
   # Services
   services = {
